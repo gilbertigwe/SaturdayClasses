200dd904c55acab3916c1b162768b2f271b3ab94
Gilbert Igwe <33848246+gilbertigwe@users.noreply.github.com>
2025-06-21 14:00:33 -0400

    Initial Commit

---
+++ 06212025_github_Actions/jokes.py
@@ -0,0 +1,12 @@
+import random
+
+jokes = [
+    "Why do programmers hate nature? It has too many bugs.",
+    "Why did the developer go broke? Because he used up all his cache.",
+    "Why do Java developers wear glasses? Because they don’t C#.",
+    "How many programmers does it take to change a light bulb? None. It’s a hardware problem.",
+    "I would tell you a UDP joke, but you might not get it."
+]
+
+print("💡 Here's a random tech joke for you:")
+print(random.choice(jokes))
